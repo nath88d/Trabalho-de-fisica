ca457fa24ccbc7e6bc51f66cf24cf136bf05e863
nath88d <104024701+nath88d@users.noreply.github.com>
2022-11-04 13:07:00 -0300

    .

--- Campo/arquivo.js
+++ Campo/arquivo.js
@@ -1,27 +1,26 @@
-/*function handleFile(files){
-    const reader = new FileReader();
-    reader.onload = (event) => {
-        let data = event.target.result;
-        document.querySelector("#texto").value = data; 
-    };
-    reader.readAsText(files);
-    console.log(reader.result.substring(0, 200));
-}*/
-var array = [];
-var array_float = [];
+var array = []; //recebe toda string do .txt separado por linha
+let separa_num = [];// Separa por elemento
+let valores_x = [];
+let valores_y = [];valores_y[0] = 0;
+let tempo = []; tempo[0] = 0;
 var openFileReadAsText = function(event) { var input = event.target;
-    var reader = new FileReader(); reader.onload = function(){ var text = reader.result;
+    var reader = new FileReader(); reader.onload = function(){
         const str = new String(reader.result);
-        document.getElementById("texto").innerHTML = reader.result;
+        //document.getElementById("texto").innerHTML = reader.result;
         array = str.split("\n");
         ler();
+        grafico(valores_x, valores_y);
     };
     reader.readAsText(input.files[0]);
 };
-function ler(){
-    for(var i = 0; i < array.length; i++){
-        array_float[i] = parseFloat(array[i].replace(",", "."))
-        console.log(array[i]+"\n");
+function ler(){// Le os dados e separa por tempo, x, y
+    for(var i = 1; i < array.length-1; i++){
+        separa_num = array[i].split("\t");
+        tempo[i] = parseFloat(separa_num[0].replace(",", "."))
+        valores_x[i] = parseFloat(separa_num[1].replace(",", "."))
+        valores_y[i] = parseFloat(separa_num[2].replace(",", "."))
+        //console.log("Tempo: " + separa_num[0] + " X: " + separa_num[1] + " Y: " + separa_num[2] +"\n");
+        //console.log("\nValor Y: " +valores_y[i]+"\n");
     }
 }
 
--- Campo/grafico.js
+++ Campo/grafico.js
@@ -1,23 +1,29 @@
-
+globalThis
 function grafico(xArray, yArray){//O numero de elementos em x temque ser o mesmo em y 
     
     // Configura os valores
     var data = [{
         x:xArray,
         y:yArray,
-        mode:"lines"
+        mode:"lines",
+        line: {
+            color: 'rgb(255, 9, 0)',
+            width: 3
+        }
     }];
     
     // Define o Layout
     var layout = {
-        xaxis: {range: [50, 160], title: "Tempo"},//Delimita o tamanho de x
-        yaxis: {range: [0, 16], title: "Velocidade"},//Delimita otamanho de y
-        title: "velocidade em funcao do tempo"
+        xaxis: {range: [0,9] ,title: "X"},//Delimita o tamanho de x
+        yaxis: {range: [0, 6], title: "Y"},//Delimita otamanho de y
+        title: "Trajetoria da bola"
     };
     
     // Display using Plotly
 Plotly.newPlot("grafico", data, layout);
 }
+
+/*
 function graficos_multiplos(){//Recebe formula por string
     var exp2 = "(x**0.1+x**2-1)-x**2";
     var exp1 = "x";
@@ -31,18 +37,18 @@ function graficos_multiplos(){//Recebe formula por string
     var y1Values = [];
     var y2Values = [];
     var y3Values = [];
-
-    for (var x = 0; x <= 10; x += 1) {
+/*
+    for(var i = 1; i < array.length; i++){
     x1Values.push(x);
     x2Values.push(x);
     x3Values.push(x);
     y1Values.push(eval(exp1));
     y2Values.push(eval(exp2));
     y3Values.push(eval(exp3));
-    }
+    }*/
 
     // Define Data
-    var data = [
+/*    var data = [
     {x: x1Values, y: y1Values, name:"Aceleracao do robo", mode:"markers"},
     {x: x2Values, y: y2Values, name:"Velocidade do robo", mode:"lines"},
     {x: x3Values, y: y3Values, name:"Velocidade da bola", mode:"lines"}
@@ -53,8 +59,5 @@ function graficos_multiplos(){//Recebe formula por string
 
     // Display using Plotly
     Plotly.newPlot("mult", data, layout);
-}
-var x = [50,60,70,80,90,100,110,120,130,140,150,90];
-var y = [7,8,8,9,5,1,10,11,14,14,15,19];
-grafico(x,y);
-graficos_multiplos();
+}*/
+//graficos_multiplos();
